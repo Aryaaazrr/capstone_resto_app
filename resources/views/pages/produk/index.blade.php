c4fd2b0e0adb600b5bc312daf41a8291628f9d16
AdeRevansa <112993224+AdeRevansa@users.noreply.github.com>
2024-05-30 20:18:58 +0700

    Add files via upload
    menambahkan view untuk produk

---
+++ resources/views/pages/produk/index.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.main_admin')
+
+@section('content')
+<main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Produk</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item active">Produk</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <h1 class="my-4">Daftar Produk</h1>
+    <a href="{{ route('produk.create') }}" class="btn btn-primary mb-3">Buat Produk Baru</a>
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <strong>{{ $message }}</strong>
+        </div>
+    @endif
+    <table class="table table-bordered">
+        <thead class="thead-dark">
+            <tr>
+                <th>Nama</th>
+                <th>Deskripsi</th>
+                <th>Harga</th>
+                <th>Kategori</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($produks as $produk)
+                <tr>
+                    <td>{{ $produk->nama_produk }}</td>
+                    <td>{{ $produk->deskripsi_produk }}</td>
+                    <td>Rp. {{ $produk->harga_produk }}</td>
+                    <td>{{ $produk->kategori->nama_kategori }}</td>
+                    <td>
+                        <a href="{{ route('produk.show', $produk->id_produk) }}" class="btn btn-info btn-sm">Tampilkan</a>
+                        <a href="{{ route('produk.edit', $produk->id_produk) }}" class="btn btn-warning btn-sm">Edit</a>
+                        <form action="{{ route('produk.destroy', $produk->id_produk) }}" method="POST" class="d-inline">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm">Hapus</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</main><!-- End #main -->
+@endsection
---
+++ resources/views/pages/produk/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.main_admin')
+
+@section('content')
+<main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Produk</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item active">Produk</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+<h1 class="my-4">Detail Produk</h1>
+    <div class="card">
+        <div class="card-header">
+            {{ $produk->nama_produk }}
+        </div>
+        <div class="card-body">
+            <p><strong>Deskripsi:</strong> {{ $produk->deskripsi_produk }}</p>
+            <p><strong>Harga:</strong> {{ $produk->harga_produk }}</p>
+            <p><strong>Kategori:</strong> {{ $produk->kategori->nama_kategori }}</p>
+            <a href="{{ route('produk.index') }}" class="btn btn-secondary">Kembali ke Daftar</a>
+        </div>
+    </div>
+</main><!-- End #main -->
+@endsection
